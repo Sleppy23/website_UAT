a76131e9705ec022b2d18d864a10f002f86b6cb5
Hector Rojas <hectortsx@outlook.com>
2025-04-07 17:49:29 -0600

    more test

--- test/afloresconstruction/test/HomePage.test.ts
+++ test/afloresconstruction/test/HomePage.test.ts
@@ -7,11 +7,6 @@ describe("@afloresconstruction Home Page Test", () => {
     await browser.url("https://www.afloresconstruction.com"); // Navigate to the website before each test
   });
 
-  it("should verify the page loads by verifying the Logo is displayed", async () => {
-    console.log("Checking if the logo is displayed...");
-    await expect(HomePage.headerLogo).toBeDisplayed();
-  });
-
   it("should verify the website loads in under 3 seconds", async () => {
     const loadTime = await browser.execute(() => {
       const [navigationEntry] = window.performance.getEntriesByType(
@@ -25,5 +20,10 @@ describe("@afloresconstruction Home Page Test", () => {
     console.log(`Page load time: ${loadTime}ms`);
     expect(loadTime).not.toBeNull(); // Ensure load time is calculated
     expect(loadTime).toBeLessThan(3000); // Assert that the load time is under 3 seconds
+  });
+
+  it("should verify the page loads by verifying the Logo is displayed", async () => {
+    console.log("Checking if the logo is displayed...");
+    await expect(HomePage.headerLogo).toBeDisplayed();
   });
 });
