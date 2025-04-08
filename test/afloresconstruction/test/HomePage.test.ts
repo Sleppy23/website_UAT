99107e83064301b8351e02031681089e3ceaac2c
Hector Rojas <hectortsx@outlook.com>
2025-04-07 18:00:11 -0600

    redid the performace test

--- test/afloresconstruction/test/HomePage.test.ts
+++ test/afloresconstruction/test/HomePage.test.ts
@@ -7,23 +7,18 @@ describe("@afloresconstruction Home Page Test", () => {
     await browser.url("https://www.afloresconstruction.com"); // Navigate to the website before each test
   });
 
-  it("should verify the website loads in under 3 seconds", async () => {
-    const loadTime = await browser.execute(() => {
-      const [navigationEntry] = window.performance.getEntriesByType(
-        "navigation"
-      ) as PerformanceNavigationTiming[]; // Explicitly cast the type
-      return navigationEntry
-        ? navigationEntry.loadEventEnd - navigationEntry.startTime
-        : null; // Time in milliseconds
-    });
-
-    console.log(`Page load time: ${loadTime}ms`);
-    expect(loadTime).not.toBeNull(); // Ensure load time is calculated
-    expect(loadTime).toBeLessThan(3000); // Assert that the load time is under 3 seconds
-  });
-
   it("should verify the page loads by verifying the Logo is displayed", async () => {
     console.log("Checking if the logo is displayed...");
     await expect(HomePage.headerLogo).toBeDisplayed();
+  });
+
+  it("should verify the website loads in under 3 seconds", async () => {
+    const loadTime = await browser.execute(() => {
+      const timing = window.performance.timing;
+      return timing.loadEventEnd - timing.navigationStart; // Time in milliseconds
+    });
+
+    console.log(`Page load time: ${loadTime}ms`);
+    expect(loadTime).toBeLessThan(3000); // Assert that the load time is under 3 seconds
   });
 });
